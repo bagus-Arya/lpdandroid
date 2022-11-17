5296a744fef6ecaed08486835ebe293e0c1d3aef
bagus-Arya <bagusarya850@gmail.com>
2022-11-17 20:07:18 +0700

    update 17 nov 2022
    - add constrain
    - register user activity

--- app/src/main/java/com/go/sispentra/DataUserActivity.kt
+++ app/src/main/java/com/go/sispentra/DataUserActivity.kt
@@ -1,11 +1,20 @@
 package com.go.sispentra
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class DataUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_user)
+        setContentView(R.layout.data_user)
+
+        val registpage_user = findViewById<Button>(R.id.btn_to_register_user)
+
+        registpage_user.setOnClickListener{
+            val intent = Intent(this@DataUserActivity, RegisterUserActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/go/sispentra/RegisterUserActivity.kt
@@ -0,0 +1,14 @@
+package com.go.sispentra
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterUserActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_user)
+
+    }
+}
