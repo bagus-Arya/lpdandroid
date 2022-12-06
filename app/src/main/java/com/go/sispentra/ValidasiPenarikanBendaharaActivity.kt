378c9eb8d9a581d2c0aa58ebb7591f3fce39d008
agum aditya <agumaditya71@gmail.com>
2022-12-06 14:35:40 +0800

    lol

--- app/src/main/java/com/go/sispentra/ValidasiPenarikanBendaharaActivity.kt
+++ app/src/main/java/com/go/sispentra/ValidasiPenarikanBendaharaActivity.kt
@@ -1,11 +1,68 @@
 package com.go.sispentra
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.rw.keyboardlistener.KeyboardUtils
 
 class ValidasiPenarikanBendaharaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bendahara_validasi_penarikan)
+
+        transparentNavigation()
+        supportActionBar?.show()
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.colorPrimary)))
+        setTitle("Validasi Penarikan")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Check Keyboard
+        KeyboardUtils.addKeyboardToggleListener(this, object :
+            KeyboardUtils.SoftKeyboardToggleListener {
+            override fun onToggleSoftKeyboard(isVisible: Boolean) {
+                if (isVisible){
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        val window = window
+                        window.clearFlags(
+                            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+
+                        )
+                        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+                        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                        //          window.statusBarColor = Color.TRANSPARENT
+//                        window.navigationBarColor = Color.TRANSPARENT
+                    }
+                }
+                else{
+                    transparentNavigation()
+                }
+                Log.d("keyboard", "keyboard visible: $isVisible")
+            }
+        })
+    }
+
+    fun transparentNavigation(){
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = window
+            window.clearFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                        or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+            )
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//            window.statusBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.TRANSPARENT
+        }
     }
 }
