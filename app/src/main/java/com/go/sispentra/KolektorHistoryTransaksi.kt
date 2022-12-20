42cbdd130d083bc58477db61c96cbec899f152e6
agum aditya <agumaditya71@gmail.com>
2022-12-21 03:04:46 +0800

    lol

--- app/src/main/java/com/go/sispentra/KolektorHistoryTransaksi.kt
+++ app/src/main/java/com/go/sispentra/KolektorHistoryTransaksi.kt
@@ -1,5 +1,7 @@
 package com.go.sispentra
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
@@ -7,20 +9,168 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.android.volley.*
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.rw.keyboardlistener.KeyboardUtils
+import com.rw.keyboardlistener.com.go.sispentra.adapter.HistoryTransaksiAdapter
+import com.rw.keyboardlistener.com.go.sispentra.adapter.TabunganTransaksiAdapter
+import com.rw.keyboardlistener.com.go.sispentra.data.LoginData
+import com.rw.keyboardlistener.com.go.sispentra.data.Transaksi
+import org.json.JSONArray
+import org.json.JSONException
 
 class KolektorHistoryTransaksi : AppCompatActivity() {
+    private var loginData= LoginData(null,null,-1)
+    private var getHistoryTransaksiUrl = "http://192.168.1.66:80/LPD_Android/public/api/transaksi/${loginData.token}"
+    private lateinit var transaksis:ArrayList<Transaksi>
+    private lateinit var mHistoryTransaksiAdapter: HistoryTransaksiAdapter
+    private lateinit var refreshLayout:SwipeRefreshLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.kolektor_history_transaksi)
+        basicStarter()
+        getAndUpdateTokenLoginData()
+        reqGetHistoryTransaksi(loginData, getHistoryTransaksiUrl)
+    }
+
+    fun createArrayTransaksis(jsonArray: JSONArray):ArrayList<Transaksi>{
+        transaksis = ArrayList<Transaksi>()
+        (0 until jsonArray.length()).forEach {
+            val transaksi = jsonArray.getJSONObject(it)
+            transaksis.add(
+                Transaksi(
+                transaksi.getInt("id"),
+                transaksi.getString("type_transaksi"),
+                transaksi.getInt("nominal"),
+                transaksi.getString("status"),
+                transaksi.getString("tgl_transaksi"),
+                transaksi.getJSONObject("bukutabungan").getString("no_tabungan"),
+                transaksi.getJSONObject("bukutabungan").getJSONObject("nasabah").getString("fullname"),
+                transaksi.getJSONObject("bukutabungan").getJSONObject("nasabah").getJSONObject("kolektor").getString("fullname")
+            )
+            )
+//            println("${book.get("book_name")} by ${book.get("author")}")
+
+        }
+        return transaksis
+    }
+
+    fun updateRv(transaksis:ArrayList<Transaksi>){
+        mHistoryTransaksiAdapter= HistoryTransaksiAdapter(transaksis)
+
+        val rv_history_transaksi=findViewById<RecyclerView>(R.id.rv_history_transaksi)
+        rv_history_transaksi.layoutManager= LinearLayoutManager(this)
+        rv_history_transaksi.adapter=mHistoryTransaksiAdapter
+
+//        mRecyclerView = findViewById<View>(R.id.recyclerView)
+//        mRecyclerView.setHasFixedSize(true)
+//        mLayoutManager = LinearLayoutManager(this)
+//        mAdapter = ExampleAdapter(mExampleList)
+//
+//        mRecyclerView.setLayoutManager(mLayoutManager)
+//        mRecyclerView.setAdapter(mAdapter)
+//
+//        mAdapter.setOnItemClickListener(object : OnItemClickListener() {
+//            fun onItemClick(position: Int) {
+//                changeItem(position, "Clicked")
+//            }
+//
+//            fun onDeleteClick(position: Int) {
+//                removeItem(position)
+//            }
+//        })
+    }
+
+    fun reqGetHistoryTransaksi(loginData: LoginData, URL:String){
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectArray: JsonArrayRequest = object : JsonArrayRequest(
+            Method.GET, URL,null,
+            Response.Listener { response ->
+                if(!response.isNull(0)){
+                    try {
+                        transaksis =createArrayTransaksis(response)
+                        updateRv(transaksis)
+                        Log.d("Res", transaksis.toString())
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
+                else{
+                    Toast.makeText(this@KolektorHistoryTransaksi, "Data Kosong", Toast.LENGTH_LONG).show()
+                }
+                refreshLayout.setRefreshing(false)
+            }, Response.ErrorListener { error ->
+                if (error is TimeoutError || error is NoConnectionError || error is NetworkError) {
+                    Toast.makeText(this@KolektorHistoryTransaksi, "Network Error", Toast.LENGTH_LONG).show()
+                    Log.d("httpfail1", error.toString())
+                } else if (error is AuthFailureError) {
+                    Log.d("httpfail2", error.toString())
+                    if(error.networkResponse.statusCode==401){
+                        val sharedPreference =  getSharedPreferences("LoginData", Context.MODE_PRIVATE)
+                        var editor = sharedPreference.edit()
+                        editor.putInt("user_id",-1)
+                        editor.putString("role",null)
+                        editor.putString("token",null)
+                        editor.commit()
+                        val intent = Intent(this@KolektorHistoryTransaksi, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                } else if (error is ServerError) {
+                    if (error.networkResponse.statusCode==403){
+                        Toast.makeText(this@KolektorHistoryTransaksi, "Forbiden", Toast.LENGTH_LONG).show()
+                    }
+                    else if (error.networkResponse.statusCode==422){
+                        Toast.makeText(this@KolektorHistoryTransaksi, "Data Input Invalid", Toast.LENGTH_LONG).show()
+                    }
+                    else if (error.networkResponse.statusCode==404){
+                        Toast.makeText(this@KolektorHistoryTransaksi, "Data Not Found", Toast.LENGTH_LONG).show()
+                    }
+//                    Toast.makeText(this@ketuaDataStaffActivity, "Server Error", Toast.LENGTH_LONG).show()
+                    Log.d("httpfail13", error.toString())
+                } else if (error is ParseError) {
+                    Toast.makeText(this@KolektorHistoryTransaksi, "Parse Error", Toast.LENGTH_LONG).show()
+                    Log.d("httpfail15", error.toString())
+                }
+                val refreshLayout = findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+                refreshLayout.setRefreshing(false)
+            }) {
+
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val map = HashMap<String, String>()
+                map["Accept"] = "application/json"
+                map["Content-Type"] = "application/json"
+                return map
+            }
+        }
+        queue.add(jsonObjectArray)
+    }
+
+    fun getAndUpdateTokenLoginData(){
+        val sharedPreference =  getSharedPreferences("LoginData", Context.MODE_PRIVATE)
+        loginData= LoginData(sharedPreference.getString("token",null),sharedPreference.getString("role",null),sharedPreference.getInt("user_id",-1))
+        getHistoryTransaksiUrl = "http://192.168.1.66:80/LPD_Android/public/api/transaksi/${loginData.token}"
+    }
+
+    fun basicStarter(){
+        refreshLayout=findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+        refreshLayout.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+            reqGetHistoryTransaksi(loginData, getHistoryTransaksiUrl)
+        })
         transparentNavigation()
         supportActionBar?.show()
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.colorPrimary)))
         setTitle("History Transaksi")
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
     }
 
     fun transparentNavigation(){
