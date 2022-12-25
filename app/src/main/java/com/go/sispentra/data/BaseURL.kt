63e2fe70e5770c9aa8b36695eb999b7e763645d3
agum aditya <agumaditya71@gmail.com>
2022-12-25 19:22:58 +0800

    Update BaseURL.kt

--- app/src/main/java/com/go/sispentra/data/BaseURL.kt
+++ app/src/main/java/com/go/sispentra/data/BaseURL.kt
@@ -1,5 +1,5 @@
 package com.rw.keyboardlistener.com.go.sispentra.data
 
 data class BaseURL(
-    var url:String?="http://192.168.1.66:80/LPD_Android/public"
+    var url:String?="https://lpdkolektor.familytransbali.com/public"
 )
