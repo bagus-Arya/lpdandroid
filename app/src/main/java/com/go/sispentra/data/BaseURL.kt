f61037660f32983e4930e69ff20f04a4873935bc
bagus-Arya <bagusarya850@gmail.com>
2022-12-28 10:30:11 +0700

    update validasi setoran
    - nambah tgl bendahara belum slese

--- app/src/main/java/com/go/sispentra/data/BaseURL.kt
+++ app/src/main/java/com/go/sispentra/data/BaseURL.kt
@@ -1,5 +1,6 @@
 package com.rw.keyboardlistener.com.go.sispentra.data
 
 data class BaseURL(
+//    var url:String?="http://192.168.1.66:80/LPD_ANDROID/public"
     var url:String?="https://lpdkolektor.familytransbali.com/public"
 )
